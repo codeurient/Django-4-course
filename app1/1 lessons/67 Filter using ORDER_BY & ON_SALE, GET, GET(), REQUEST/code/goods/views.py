778a44b18cdeed51d3e64591dbf9010ea83ffd8d
Mehdi Barasov <asker.20tr@gmail.com>
2024-06-02 21:47:50 +0400

    save filter values

--- app1/1 lessons/67 Filter using ORDER_BY & ON_SALE, GET, GET(), REQUEST/code/goods/views.py
+++ app1/1 lessons/67 Filter using ORDER_BY & ON_SALE, GET, GET(), REQUEST/code/goods/views.py
@@ -5,6 +5,7 @@ from goods.models import Products
 
 def catalog(request, category_slug):
     page     = request.GET.get('page', 1)
+    # 1) REQUEST-den deyerleri elde etmek ucun GET() metodundan istifade edirik. Elde edilen deyerleri VARIABLE-llara yerlesdiririk. Sonra IF ile sorgu yaradaraq uyğun PRODUCT-u göstər deyirik. 
     on_sale  = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
 
@@ -13,8 +14,11 @@ def catalog(request, category_slug):
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug = category_slug))
 
+    # 2) __GT (greater than) demekdir. Yəni DB-de ki DISCOUNT sahesi 0-dan boyuk olanlari filtirləsin demişik. Bu sadəcə endirimdə olan məhsulları əldə etməyimizə köməklik göstərəcək. 
     if on_sale:
         goods = goods.filter(discount__gt=0)
+    # 3) ORDER_BY() metodunun içində PRİCE sütunun adını yazaraq qiymətə görə sırala deyirik. Əgər PRİCE stringinin önünə - (mənfi) qoysaq onda azalan sıra ilə sıralamış olacağıq. 
+    # DEFAULT deyilən bir sütun olmadığı üçün ORDER_BY != 'DEFAULT' yazdıq ki, əgər həmin İNPUT seçilərsə onda İF işləməyəcək. 
     if order_by and order_by != "default":
         goods = goods.order_by(order_by)
 
---
+++ app1/1 lessons/67 Filter using ORDER_BY & ON_SALE, GET, GET(), REQUEST/code/main/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request) -> HttpResponse:
+    context = {
+        'title' : 'Home - main',
+        'content' : 'Furniture store HOME',
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'title' : 'Home - about',
+        'content' : 'Here is some text about us',
+        'text_on_page': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ',
+    }
+    return render(request, 'main/about.html', context)
