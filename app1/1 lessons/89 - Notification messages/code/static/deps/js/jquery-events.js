c9b39231f3072c3c0c4d5137ce7ad7cd21b41d6c
Mehdi Barasov <asker.20tr@gmail.com>
2024-07-27 09:39:15 +0400

    code

--- app1/1 lessons/89 - Notification messages/code/static/deps/js/jquery-events.js
+++ app1/1 lessons/89 - Notification messages/code/static/deps/js/jquery-events.js
@@ -1,7 +1,13 @@
+// 1) Burda JS-in Jquery adlı kitabxanasından istifadə etmişik. 
 $(document).ready(function () {
+    // 2) Elementi İD-si ilə əldə edirik. Bu Django nun xəbərdarlıq mesajını göstərən pəncərədir. Həmin elementi konsola yazdırdıqda görərik ki onun LENGTH
+    //    adında bir parametri var və dəyəri birdir. 
     var notification = $('#notification');
+    // 3) Buna görə də NOTİFİCATİON.LENGTH ilə 1-i əldə edərək 0 (sıfır)-la müqayisə edirik və nəticə TRUE olacağı üçün İF işləyəcək.
     if (notification.length > 0) {
+        // 4) 7 saniyə sonra isə pəncərəni qapadırıq.
         setTimeout(function () {
+            // 5) Pencereni qapatmaq ucun ise Bootstrap-in ALERT('close') metodundan istifadə edirik.
             notification.alert('close');
         }, 7000);
     }   
--- app1/1 lessons/89 - Notification messages/code/users/views.py
+++ app1/1 lessons/89 - Notification messages/code/users/views.py
@@ -15,6 +15,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                # 2) LOGIN olduqdan sonra istifadeciye mesaj gostermek ucun MESSAGES modulundan istifade ede bilerik:                                           from django.contrib import auth, messages
                 messages.success(request, f"{username}, You are now logged in")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
@@ -26,8 +27,6 @@ def login(request):
     return render( request, 'users/login.html', context )
 
 
-
-
 def registration(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
@@ -35,6 +34,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            # 3) Eyni mesaji qeydiyyatdan kecdikden sonra da gostermek ucun burada da yaziriq.
             messages.success(request, f"{user.username}, You are now registered and logged in")
             return HttpResponseRedirect(reverse('main:index'))
     else:
@@ -46,17 +46,24 @@ def registration(request):
     return render( request, 'users/registration.html', context )
 
 
-
-
 @login_required
 def profile(request):
     if request.method == 'POST':
         form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
         if form.is_valid():
+            # 4) Ve UPDATE etdikden sonra da mesaj gostermek ucun yaziriq
             messages.success(request, "Profile successfully updated")
             form.save()
             return HttpResponseRedirect(reverse('user:profile'))
     else:
+        # 1) Saytan LOG OUT olduqdan sonra URL yerinde bele yazdiqda AttributeError xeta mesaji ile qarsilasiriq: http://127.0.0.1:8000/user/profile/
+        # Bunun PROFILEFORM() klasinda yazdigimiz INSTANCE parametridir. Cunki bu parametr USER modelinden melumatlari input sahelerinde eks etdirmek ucun istifade edilir.
+        # Ancaq biz PROFILE sehifesini ve hemin melumatlari ancaq LOGIN olduqda gore bilmeliyik. 
+        # 
+        # LOGOUT olduqdan sonra qeydiyyatdan kecmiyen yaxud sayta daxil olmuyan istifadecilerə bezi controller-lere daxil olmani qisitlamaq lazimdir. 
+        # 
+        # Bunu etmek ucun LOGİN_REQUİRED adli Dekaratordan istifade ede bilerik. Hemin dekaratoru yuxarida IMPORT etmek lazimdir:                   from django.contrib.auth.decorators import login_required
+        # Artiq xeta mesaji evezine PAGE NOT FOUND goreceyik. Bu normaldir. 
         form = ProfileForm(instance=request.user)
     context = {
         'title' : ' Home - Profile ',
@@ -66,9 +73,8 @@ def profile(request):
 @login_required
 
 
-
-
 def logout(request):
+    # 5) Cixis etdikde de mesaj gostermek ucun yaziriq.   Bunlari yazdiqdan sonra daxil oluruq TEMPLATES - INCLUDES - NOTIFICATIONS.HTML faylina. Ve bu fayla lazimli HTML tag-lerimizi yaziriq ki, CIXIS ve.s etdikde gosterilecek alert mesajları hemin tag-lere yazdirilsin ve bize gösterilsin.
     messages.success(request, f"{request.user.username}, You are now logged out")
     auth.logout(request)
     return redirect(reverse('main:index'))
